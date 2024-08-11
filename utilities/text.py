b3cbbdd6f8bc77fc8abbd048cc44161f1f153f6d
Marko Djordjic <marko.djordjic@outlook.com>
2024-08-11 10:26:19 +0200

    Merge w/ text loader.

--- utilities/text.py
+++ utilities/text.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 class TextLoader:
@@ -10,18 +11,40 @@ class TextLoaderManager:
 
     def __init__(self, folder: str = None) -> None:
         self.folder = Path(folder)
-        self._file_names: list [str] = None
-        self._pieces_of_text: list[str] = None
+        self._file_names: list [str] = []
+        self._pieces_of_text: list[str] = []
 
-    def _get_files_from_folder(self):
+    def _get_file_names_from_folder(self):
         
         assert self.folder is not None, 'No folder to read the data.'
 
+        self._file_names = [
+            file for file in os.listdir(self.folder)
+            if os.path.isfile(os.path.join(self.folder, file))
+        ]
+
+    @staticmethod
+    def _read_file(complete_file_name: Path = None):
+        with open(complete_file_name, 'r', encoding='utf8') as text_file:
+            content = text_file.readlines()
+
+        return content
+
     def _read_files(self):
-        pass
+        
+        assert self._file_names is not None, 'No files to read the data.'
+
+        for file_name in self._file_names:
+            complete_file_name = Path(self.folder, file_name)
+            content = self._read_file(complete_file_name=complete_file_name)
+            self._pieces_of_text.extend([content])
+
+    def read_files(self):
+        self._get_file_names_from_folder()
+        self._read_files()
 
     def get_pieces_of_text(self):
 
-        assert self._pieces_of_text is not None, 'No text has been read.'
+        assert len(self._pieces_of_text) > 0, 'No text has been read.'
 
         return self._pieces_of_text
