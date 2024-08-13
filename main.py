b4a06987ee66ae4dd4fe4b7e8bd38ee8cbe373a3
Marko Djordjic <marko.djordjic@outlook.com>
2024-08-13 12:00:14 +0200

    Merge w/ text summarizer.

--- main.py
+++ main.py
@@ -1,14 +1,88 @@
+# %% [markdown]
+# # Summarization of Large Pieces of Text with LangChain 
+# ## Context
+# Modern NLP systems excel at text summarization--extraction of features
+# which retain the meaning of the piece of text, as well as 
+# reformulating them into *new* sentences. However, as the task grows in 
+# complexity because of inherent limitations of the LLMs to consume, and 
+# timely process the text, it is necessary to create a specific strategy 
+# to perform summarization of larger pieces of text such as whole books.
+# ## Objective
+# As partially revealed in previous paragraph the objective of this demo
+# is to create a summary of the text that is several times greater
+# than the current limitation in terms of tokens that can be consumed
+# by LLMs. While, the only limitation appears to be number of tokens,
+# other limitations need to be taken into account as well: cost, speed,
+# overall quality in order to meet the objective.
+# ## Technological Stack
+# The back-end of the whole system is the LangChain library. However,
+# its scope will be limited to utilization of only some classes, such as:
+# `RecursiveCharacterSplitter`, `PromptTemplate`, and `Document`. While,
+# `LangChain` does provides classes to perform summarization of large
+# pieces of text into chains, there is always a benefit in balancing
+# between the utilization of pre-existing classes and custom 
+# implementation. In this case custom implementation will handle the 
+# splitting of the text and further summarization chain. The choice of
+# the LLM has fallen to `OpenAI`s GPT-3.5-Turbo which can consume 4096.
+# tokens. The compete system is monitored via `LangSmith` platform.
+# ## Methodology
+# In order to achieve the objective a following methodology has been
+# selected:
+#   1. A selection of large pieces of text will be made on the basis of
+#   publicly available books in txt format from the
+#   https://www.gutenberg.org/.
+#   2. Data from these txt files will be loaded as is, and split into
+#   sections. The approach for splitting is that regardless of the
+#   initial text size, text will always be split to no more than 128
+#   sections, which will be of the same size within the document, and
+#   can be of different size across different documents.
+#   3. Summarization of the sections will be done by aggregating two
+#   consecutive pieces-of-text in a binary fashion until all pairs are
+#   exhausted.
+#   4. Final summary will be an agglomeration of all previous pairs, and
+#   within formation of intermediate summaries restrictions will be
+#   imposed in terms of number of paragraphs and length of each 
+#   paragraph expressed in total number of characters including the
+#   whitespaces. Hence, the explanation for not completely relying on
+#   `LangChain` classes.
+# ## Architecture
+# Having in mind that complexity of the task lies in the whole system
+# design, the architecture of the LLM sub-system is fairly simple. It
+# is a LLM based on a custom prompt containing instructions on how to
+# summarize two pieces of text.
+# ## Implementation
+# Firstly, let us make the necessary imports.
+# %%
 from utilities.text import TextLoaderManager
-from summarizer.summarizer import TextSplitterManager
-
-if __name__ == '__main__':
-
-    TEXT_FOLDER = r'C:\Users\marko\Desktop\data'
-
-    text_loader = TextLoaderManager(folder=TEXT_FOLDER)
-    text_loader.read_files()
-    texts = text_loader.get_pieces_of_text()
-
-    text_splitter = TextSplitterManager(pieces_of_text=texts)
-    text_splitter.split_documents()
-    text_splitter.get_split_documents()
+from summarizer.summarizer import TextSplitterManager, SummarizationManager, \
+    TextPlotterManager
+# %% [markdown]
+# Secondly, let us delare a file handle for the folder with the data.
+# %%
+TEXT_FOLDER = r'C:\Users\marko\Desktop\data'
+# %% [markdown]
+# Now let us utilize the `TextLoaderManager` class to load the text 
+# files.
+# %%
+text_loader = TextLoaderManager(folder=TEXT_FOLDER)
+text_loader.read_files()
+texts = text_loader.get_pieces_of_text()
+# %% [markdown]
+# The next step is to split the documents, according to the 
+# methodological specification.
+# %%
+text_splitter = TextSplitterManager(pieces_of_text=texts)
+text_splitter.split_documents()
+split_documents = text_splitter.get_split_documents()
+# %% [markdown]
+# Finally it is possible to start the `SummarizationManager` and to
+# create summaries of documents.
+# %%
+summarizer = SummarizationManager(chuncked_pieces_of_text=[split_documents])
+summarizer.summarize_text()
+summarized_text = summarizer.get_summarized_texts()
+# %% [markdown]
+# Let us take a look at the text summaries.
+# %%
+text_plotter = TextPlotterManager(documents=summarized_text)
+text_plotter.plot_text()
--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -1,23 +1,128 @@
+from copy import deepcopy
+import textwrap
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as grid
+import matplotlib
+matplotlib.use('TkAgg')
 from langchain_core.documents import Document
+from langchain_core.prompts import PromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from utilities.general import environment_reader
+
+environment = environment_reader('./.env')
 
 class Summarizer:
 
-    def __init__(self) -> None:
-        pass
+    llm = ChatOpenAI(
+        model='gpt-3.5-turbo',
+        api_key=environment['OPENAI_API_KEY'],
+        temperature=0
+    )
 
-class SummarizerManager:
+    template = """You are an summarization assistant. Your job is
+        to summarize two pieces of text into a single piece of text.
+        Pay attention to people names in the original pieces of text,
+        when summarizing them. Here is the first piece of text 
+        {first_text} and here is the second piece of text {second_text}.
+        The summary you make must be a complete text on its own. It 
+        cannot consist of statements "The first piece of text", 
+        "The second piece of text". Final summary cannot have more than 
+        four paragraphs. Each paragraph cannot be longer than 448 
+        characters including white spaces. Terminate each paragraph with `\n\n`.
+    """
 
-    def __init__(self, chuncked_pieces_of_text) -> None:
+    prompt = PromptTemplate.from_template(template)
+    
+    chain = prompt | llm
 
-        self.chuncked_pieces_of_text
+    def __init__(self, chuncked_piece_of_text) -> None:
+        self.chucked_piece_of_text = chuncked_piece_of_text
+        self._summarized_text = deepcopy(chuncked_piece_of_text)
+        self._summarization_pairs = None
+
+    @staticmethod
+    def _create_summarization_pairs(pairs):
+        first_list = list(range(0, len(pairs))) 
+        second_list = []
+        for i in range(0, len(first_list)-1, 2):
+            if first_list[i]+1 == first_list[i+1]:
+                second_list.append([first_list[i], first_list[i+1]])
+
+        return second_list
+    
+    @staticmethod
+    def _log_message(cycle, index):
+        if index%4 == 0:
+            print(f'Summary cycle: {cycle}, pair: {index}')
+    
+    def _get_pieces_of_text(self, pair):
+
+        return self._summarized_text[pair[0]], self._summarized_text[pair[1]]
+
+    def _summarize_single_pair(self, pair: list = []) -> Document:
+        first_piece_of_text, second_piece_of_text = \
+            self._get_pieces_of_text(pair)        
+        result = self.chain.invoke({
+            'first_text': first_piece_of_text,
+            'second_text': second_piece_of_text
+        })
+        document = Document(page_content=result.content)
+
+        return document
+
+    def _summarize_pairs_within_cycle(self, cycle_index) -> list[str]:
+        new_texts = []
+        for pair_index, pair in enumerate(self._summarization_pairs):
+            self._log_message(cycle=cycle_index, index=pair_index)
+            document = self._summarize_single_pair(pair=pair)
+            new_texts.extend([document])
+
+        return new_texts
+
+    def _summarize(self):
+        cycle = 1
+        while len(self._summarized_text) > 1:
+            self._summarization_pairs = \
+                self._create_summarization_pairs(pairs=self._summarized_text)
+            summary_cycle = \
+                self._summarize_pairs_within_cycle(cycle_index=cycle)
+            cycle = cycle + 1
+            self._summarized_text = summary_cycle
         
+        print('Summarization complete.')       
+
+    def summarize(self):
+        self._summarize()
+
+    def get_summarized_text(self):
+
+        return self._summarized_text
+
+class SummarizationManager:
+
+    def __init__(self, chuncked_pieces_of_text: list = None) -> None:
+        self.chuncked_pieces_of_text = chuncked_pieces_of_text
+        self._summarized_text = []
+
+    def summarize_text(self):
+        for index, chuncked_text in enumerate(self.chuncked_pieces_of_text):
+            print(f'Summarizing {index+1} out of {len(self.chuncked_pieces_of_text)} documents.')
+            summarizer = Summarizer(chuncked_piece_of_text=chuncked_text)
+            summarizer.summarize()
+            self._summarized_text.extend([summarizer.get_summarized_text()])           
+
+    def get_summarized_texts(self):
+        
+        assert self._summarized_text is not None, 'No summarized text.'
+
+        return self._summarized_text
 
 
 class TextSplitter:
 
     def __init__(self, 
-                 chunk_size: int = 384, 
+                 chunk_size: int = 448, 
                  document: str = None) -> None:
         self.chunk_size = chunk_size
         self.document = document
@@ -30,10 +135,17 @@ class TextSplitter:
             chunk_size=self.chunk_size, 
             chunk_overlap=0
         )
+
     def _split_document(self):
-        self._converted_document = self._splitter.create_documents(
-            [self.document]
-        )
+        self._converted_document = \
+            self._splitter.create_documents([self.document])
+        chunks = len(self._converted_document)
+        while chunks > 128:
+            self.chunk_size += 32
+            self._instantiate_splitter()
+            self._converted_document = \
+                self._splitter.create_documents([self.document])
+            chunks = len(self._converted_document)
 
     def split_document(self):
         
@@ -48,6 +160,7 @@ class TextSplitter:
     
         return self._converted_document
 
+
 class TextSplitterManager:
 
     def __init__(self, pieces_of_text: list[str] = None) -> None:
@@ -60,7 +173,7 @@ class TextSplitterManager:
         text_splitter.split_document()
         converted_text = text_splitter.get_converted_document()
         print(
-            f'Text with: {len(text)} characters, split into {len(converted_text)} pieces.'
+            f'Text with: {len(text)} characters, split into {len(converted_text)} pieces with chunk size: {text_splitter.chunk_size}.'
         )
 
         return converted_text
@@ -78,3 +191,73 @@ class TextSplitterManager:
         assert len(self._split_documents) > 0, 'No split documents.'
 
         return self._split_documents
+
+
+class TextPlotter:
+
+    text_wrapper = textwrap.TextWrapper(width=70, fix_sentence_endings=True)
+
+    def __init__(self, document: list[str] = None) -> None:
+        self.paragraphs = document
+        self._splited_paragraphs = None
+        self._wrapped_paragraphs = None
+        self._joined_paragraphs = None
+
+    def _split_paragraphs(self):
+        self._splited_paragraphs = \
+            self.paragraphs[0].page_content.split('\n\n')
+
+    def _wrap_paragraph_text(self, paragraph):
+        wrapped = self.text_wrapper.wrap(text=paragraph)
+        joined = '\n'.join(wrapped)
+
+        return joined
+
+    def _wrap_text(self):
+        wrapped_paragraphs = []
+        for paragraph in self._splited_paragraphs:
+            wrapped_paragraph = self._wrap_paragraph_text(paragraph=paragraph)
+            wrapped_paragraphs.extend([wrapped_paragraph])
+
+        self._wrapped_paragraphs = wrapped_paragraphs
+
+    def _join_paragraphs(self):
+        self._joined_paragraphs = '\n\n'.join(self._wrapped_paragraphs)
+
+    def _plot_text(self):
+        figure = plt.figure(figsize=[8.3, 11.7])
+        plotting_grid = grid.GridSpec(nrows=1, ncols=1)
+        axis_1 = figure.add_subplot(plotting_grid[0, 0])
+        text_kwargs = \
+            dict(ha='left', va='top', fontsize=10, family='Times New Roman')
+        axis_1.text(x=.05, y=.95, s=self._joined_paragraphs, **text_kwargs)  
+        axis_1.set_xticklabels([])
+        axis_1.set_yticklabels([])
+        axis_1.tick_params(
+            axis='both', 
+            which='both', 
+            bottom=False, 
+            top=False, 
+            left=False
+        )
+        plt.show()
+    
+    def plot_text(self):
+        self._split_paragraphs()
+        self._wrap_text()
+        self._join_paragraphs()
+        self._plot_text()
+
+class TextPlotterManager:
+
+    def __init__(self, documents: list[str]) -> None:
+        self.documents = documents
+
+    @staticmethod
+    def _plot_text(document: list[str] = None):
+        text_plotter = TextPlotter(document=document)
+        text_plotter.plot_text()
+
+    def plot_text(self):
+        for document in self.documents:
+            self._plot_text(document=document)
